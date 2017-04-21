9c2af7e0fa1afca0da8b439e35b816496cab5077
U_021LY <u_021ly@MacBook-Pro-U-021LY.local>
2017-04-21 21:23:18 +0300

    feat(*): fixes according @greendizer recomendations

--- arui-demo/components/preview-with-theme-switcher/index.jsx
+++ arui-demo/components/preview-with-theme-switcher/index.jsx
@@ -1,60 +1,54 @@
 import React from 'react';
 import Type from 'prop-types';
+import { autobind } from 'core-decorators';
 import ThemeProvider from '../../../src/theme-provider/theme-provider';
 import cn from '../../../src/cn';
 import './index.css';
+
+const lightTheme = 'alfa-on-white';
+const darkTheme = 'alfa-on-color';
 
 @cn('preview-with-theme-switcher')
 class PreviewWithThemeSwitcher extends React.Component {
     static propTypes = {
         children: Type.oneOfType([Type.arrayOf(Type.node), Type.node])
     };
-
     state = {
-        theme: 'alfa-on-white'
+        theme: lightTheme
     }
-
-    handleOnChange = (theme) => {
-        this.setState({ theme });
-    }
-
     render(cn) {
-        const themes = [
-            {
-                text: 'Светлая',
-                value: 'alfa-on-white',
-                demoSectionPrefix: 'alfa-on-color',
-                themeProviderPrefix: 'alfa-on-white'
-            }, {
-                text: 'Темная',
-                value: 'alfa-on-color',
-                demoSectionPrefix: 'alfa-on-white',
-                themeProviderPrefix: 'alfa-on-color'
-            }
-        ];
-        const activeTheme = themes.filter(theme => theme.value === this.state.theme)[0];
+        const themes = [lightTheme, darkTheme];
         return (
-            <div className={ cn('') }>
-                <div className={ cn(activeTheme.demoSectionPrefix) }>
-                    <ThemeProvider theme={ activeTheme.themeProviderPrefix }>
-                        <div>
-                            <div className={ cn('button-group') } >
-                                {themes.map(({ value: theme, text }) => (
-                                    <button
-                                        key={ theme }
-                                        className={ cn('button') }
-                                        onClick={ () => this.handleOnChange(theme) }
-                                    >
-                                        { text }
-                                    </button>
-                                ))}
-                            </div>
+            <div>
+                <div className={ cn('button-group') } >
+                    {themes.map(theme => (
+                        <button
+                            key={ theme }
+                            className={ cn('button') }
+                            onClick={ () => this.handleOnChange(theme) }
+                        >
+                            { this.renderThemeText(theme) }
+                        </button>
+                    ))}
+                </div>
+                <div className={ cn('layout', { theme: this.state.theme }) } >
+                    <ThemeProvider theme={ this.state.theme } >
+                        <div className={ cn('wrapper') }>
                             {this.props.children}
                         </div>
                     </ThemeProvider>
                 </div>
             </div>
         );
+    }
+
+    renderThemeText(theme) {
+        return theme === lightTheme ? 'светлая' : 'темная';
+    }
+
+    @autobind
+    handleOnChange(theme) {
+        this.setState({ theme });
     }
 }
 export default PreviewWithThemeSwitcher;
--- styleguide.config.js
+++ styleguide.config.js
@@ -9,7 +9,6 @@ const ARUI_TEMPLATE = require('arui-presets/webpack.base');
 
 module.exports = {
     title: 'ARUI FEATHER',
-    template: path.resolve(__dirname, './arui-demo/demo.html'),
     serverPort: 3013,
     skipComponentsWithoutExample: true,
     components: 'src/**/**/[a-z]*.jsx',
@@ -18,8 +17,8 @@ module.exports = {
     webpackConfig: merge.smart(ARUI_TEMPLATE, {
         resolve: {
             alias: {
-                // Override styleguidist preview template for adding theme switcher functionality
-                'rsg-components/Preview': path.resolve(__dirname, './arui-demo/components/preview.jsx'),
+                // Переопределяем компоненты styleguidist
+                'rsg-components/Wrapper': path.resolve(__dirname, './arui-demo/components/preview-with-theme-switcher'),
                 'rsg-components/Logo': path.resolve(__dirname, './arui-demo/components/logo.jsx')
             }
         }

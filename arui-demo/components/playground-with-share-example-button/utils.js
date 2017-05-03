e4a0b9a2de3cce9138d2adfe208d73e8b988a832
stepancar <u_021ly@MacBook-Pro-U-021LY.local>
2017-05-03 03:16:33 +0300

    feat(playground): add playground to demo, implement share example functionality

---
+++ arui-demo/components/playground-with-share-example-button/utils.js
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export function getPlayground(sections) {
+    const evalInContext = sections[0].components[0].props.examples.filter(example =>
+    example.evalInContext)[0].evalInContext;
+    const code = decodeURI(location.hash.replace('#playground/code=', ''));
+    sections = [{
+        components: [{
+            name: 'PlayGround',
+            hasExamples: true,
+            props: {
+                examples: [{
+                    content: code,
+                    type: 'code',
+                    evalInContext
+                }],
+                methods: []
+            }
+        }]
+    }];
+    return sections;
+}
+
+export function isPlayground(hash = window.location.hash) {
+    return hash.indexOf('#playground/code=') !== -1;
+}
---
+++ arui-demo/components/styleguide/index.jsx
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* eslint import/no-extraneous-dependencies: [2, {"devDependencies": true}] */
+import StyleGuide from 'react-styleguidist/lib/rsg-components/StyleGuide';
+import {
+    processSections,
+    setSlugs,
+    slugger
+} from 'react-styleguidist/lib/utils/utils';
+import { getPlayground, isPlayground } from '../playground-with-share-example-button/utils';
+
+export default function (props) {
+    if (isPlayground()) {
+        /* eslint global-require: 0 */
+        /* eslint import/no-webpack-loader-syntax: 0 */
+        /* eslint import/no-unresolved: 0 */
+        const styleguide = require('!!react-styleguidist/loaders/styleguide-loader!react-styleguidist/lib/index.js');
+        // Reset slugger for each render to be deterministic
+        slugger.reset();
+        const sections = setSlugs(getPlayground(processSections(styleguide.sections))); // подменяем секции для отображения
+        /* eslint react/prop-types: 0 */
+        const config = { ...props.config, showCode: true };
+        return (
+            <StyleGuide { ...props } config={ config } sections={ sections } isolatedComponent={ true } />
+        );
+    }
+
+    return (
+        <StyleGuide { ...props } />
+    );
+}
+

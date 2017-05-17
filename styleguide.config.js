49cc2edbedf8867f5542a8834e2fbc3e71db039f
Stepan Mikhaylyuk <stepancar@hotmail.com>
2017-05-17 18:46:28 +0300

    feat(playground): add share example functionality (#23)
    * feat(playground): add playground to demo, implement share example functionality

--- styleguide.config.js
+++ styleguide.config.js
@@ -19,7 +19,17 @@ module.exports = {
             alias: {
                 // Переопределяем компоненты styleguidist
                 'rsg-components/Wrapper': path.resolve(__dirname, './arui-demo/components/preview-with-theme-switcher'),
-                'rsg-components/Logo': path.resolve(__dirname, './arui-demo/components/logo.jsx')
+                'rsg-components/Logo': path.resolve(__dirname, './arui-demo/components/logo.jsx'),
+                'rsg-components/Playground/PlaygroundRenderer': path.resolve(__dirname,
+                    './arui-demo/components/playground-with-share-example-button'
+                ),
+                'rsg-components/StyleGuide/StyleGuideRenderer': path.resolve(__dirname,
+                    './node_modules/react-styleguidist/lib/rsg-components/StyleGuide/StyleGuideRenderer'
+                ),
+                'rsg-components/StyleGuide/index': path.resolve(__dirname,
+                    './node_modules/react-styleguidist/lib/rsg-components/StyleGuide/index'
+                ),
+                'rsg-components/StyleGuide': path.resolve(__dirname, './arui-demo/components/styleguide')
             }
         }
     })

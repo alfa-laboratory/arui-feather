f0a4d6e985bf4d3e89d7a55145962d4516ab18fd
stepancar <u_021ly@MacBook-Pro-U-021LY.local>
2017-05-25 03:13:37 +0300

    feat(*): add docs for composed componenets

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,13 +5,39 @@
 
 const path = require('path');
 const merge = require('webpack-merge');
+const reactDockGen = require('react-docgen');
+const fs = require('fs');
 const ARUI_TEMPLATE = require('arui-presets/webpack.base');
+
+const documentation = { };
+function reactDocGenWithMergedComposed(filePath, resolver, handlers) {
+    if (documentation[filePath]) {
+        return documentation[filePath];
+    }
+    const content = fs.readFileSync(filePath);
+    const doc = reactDockGen.parse(content, resolver, handlers)[0];
+    doc.filePath = filePath;
+    if (doc.composes) {
+        doc.composes = doc.composes.map((relativePath) => {
+            const composeComponentPath = path.resolve(path.dirname(filePath), `${relativePath}.jsx`);
+            return reactDocGenWithMergedComposed(composeComponentPath, resolver, handlers);
+        });
+    } else {
+        doc.composes = [];
+    }
+    doc.props = doc.composes.reduce((prev, item) => Object.assign({}, prev, item.props), doc.props || {});
+    documentation[filePath] = doc;
+    return doc;
+}
 
 module.exports = {
     title: 'ARUI FEATHER',
     serverPort: 3013,
     skipComponentsWithoutExample: true,
     components: 'src/**/**/[a-z]*.jsx',
+    propsParser(filePath, source, resolver, handlers) {
+        return reactDocGenWithMergedComposed(filePath, resolver, handlers);
+    },
     ignore: ['**/*-test.jsx'],
     styleguideDir: path.resolve(__dirname, './arui-demo/styleguide/'),
     webpackConfig: merge.smart(ARUI_TEMPLATE, {

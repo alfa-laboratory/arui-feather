7c565e2fc9df76cdc6231813816fc8c907404efa
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-06-09 14:17:51 +0300

    feat(fantasy): change heading default font weight from black to bold

--- src/textarea/fantasy/index.js
+++ src/textarea/fantasy/index.js
@@ -2,4 +2,4 @@ import './textarea.css';
 import './textarea_theme_alfa-on-color.css';
 import './textarea_theme_alfa-on-white.css';
 
-export default from './textarea';
+export default from '../textarea';

38dad9c98048d0ef9e9316eea049396c5eafbccf
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-06-15 22:02:10 +0300

    refactor(menu-item): update paths

--- src/menu-item/fantasy/index.js
+++ src/menu-item/fantasy/index.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import '../../dropdown';
-import '../../link';
+import '../../dropdown/fantasy';
+import '../../link/fantasy';
 
 import './menu-item.css';
 

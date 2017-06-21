ed234aadb1b7ab113730022818da5af2ff87846c
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-06-21 22:47:54 +0300

    test(*): remove waste eslint-disable

--- src/header/header-test.jsx
+++ src/header/header-test.jsx
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint import/no-extraneous-dependencies: [2, {"devDependencies": true}] */
-
 import { render, cleanUp, simulate } from '../test-utils';
 
 import Header from './header';

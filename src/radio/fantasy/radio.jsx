0eaa65316160f0412ac0abb32bc6dd169ceec092
Alexandr Kitov <akitov@alfabank.ru>
2017-08-03 17:00:30 +0300

    fix(radio): rename fantasy component

--- src/radio/fantasy/radio.jsx
+++ src/radio/fantasy/radio.jsx
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Radio from '../radio';
+import OriginalRadio from '../radio';
 import TagButton from '../../tag-button/fantasy/tag-button';
 
 import cn from '../../cn';
@@ -10,9 +10,9 @@ import cn from '../../cn';
 /**
  * Компонент радио-кнопки в обновлённом дизайне.
  *
- * @extends Radio
+ * @extends OriginalRadio
  */
 @cn('radio', TagButton)
-class FantasyRadio extends Radio {}
+class Radio extends OriginalRadio {}
 
-export default FantasyRadio;
+export default Radio;

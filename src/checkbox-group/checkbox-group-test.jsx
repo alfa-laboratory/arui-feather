bb34ff4d9f99ed20dfe1429a83cc4ea305afc9ab
ziimir <ziimir9@gmail.com>
2017-08-18 11:50:18 +0300

    feat(*): checkboxgroup now can control child checkboxes (#159)

--- src/checkbox-group/checkbox-group-test.jsx
+++ src/checkbox-group/checkbox-group-test.jsx
@@ -2,25 +2,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { render, cleanUp } from '../test-utils';
+import { render, cleanUp, simulate } from '../test-utils';
 
-import ChecBoxGroup from './checkbox-group';
-import ChecBox from '../checkbox/checkbox';
+import CheckBoxGroup from './checkbox-group';
+import CheckBox from '../checkbox/checkbox';
 
 describe('checkbox-group', () => {
     afterEach(cleanUp);
 
     it('should render without children', () => {
-        let checkboxGroup = render(<ChecBoxGroup />);
+        let checkboxGroup = render(<CheckBoxGroup />);
 
         expect(checkboxGroup.node).to.exist;
     });
 
     it('should render with only one children', () => {
         let checkboxGroup = render(
-            <ChecBoxGroup>
-                <ChecBox key='1' />
-            </ChecBoxGroup>
+            <CheckBoxGroup>
+                <CheckBox key='1' />
+            </CheckBoxGroup>
         );
 
         expect(checkboxGroup.node).to.exist;
@@ -30,14 +30,151 @@ describe('checkbox-group', () => {
 
     it('should render with many checkbox children without problems', () => {
         let checkboxGroup = render(
-            <ChecBoxGroup>
-                <ChecBox key='1' text='label' />
-                <ChecBox key='2' text='label' />
-            </ChecBoxGroup>
+            <CheckBoxGroup>
+                <CheckBox key='1' text='label' />
+                <CheckBox key='2' text='label' />
+            </CheckBoxGroup>
         );
 
         expect(checkboxGroup.node).to.exist;
         expect(checkboxGroup.node).to.have.class('checkbox-group');
         expect(checkboxGroup.node).to.have.class('control-group');
+    });
+
+    it('should render checked checkbox with value from `value` props', () => {
+        let checkboxGroup = render(
+            <CheckBoxGroup
+                value={ ['value_1'] }
+            >
+                <CheckBox value='value_1' text='label 1' />
+                <CheckBox value='value_2' text='label 2' />
+            </CheckBoxGroup>
+        );
+        let checkboxNode = checkboxGroup.node.querySelector('.checkbox');
+
+        expect(checkboxNode).to.have.class('checkbox_checked');
+    });
+
+    it('should focus first child checkbox-button on public focus method', (done) => {
+        let checkboxGroup = render(
+            <CheckBoxGroup>
+                <CheckBox />
+                <CheckBox />
+                <CheckBox />
+            </CheckBoxGroup>
+        );
+        let firstCheckboxNode = checkboxGroup.node.querySelectorAll('input[type="checkbox"]')[0];
+
+        checkboxGroup.instance.focus();
+
+        setTimeout(() => {
+            expect(document.activeElement === firstCheckboxNode).to.be.equal(true);
+            done();
+        }, 0);
+    });
+
+    it('should lose focus on public blur method', (done) => {
+        let checkboxGroup = render(
+            <CheckBoxGroup>
+                <CheckBox />
+                <CheckBox />
+                <CheckBox />
+            </CheckBoxGroup>
+        );
+        let firstCheckboxNode = checkboxGroup.node.querySelectorAll('input[type="checkbox"]')[0];
+
+        checkboxGroup.instance.focus();
+
+        setTimeout(() => {
+            expect(document.activeElement === firstCheckboxNode).to.be.equal(true);
+
+            checkboxGroup.instance.blur();
+
+            setTimeout(() => {
+                expect(document.activeElement === firstCheckboxNode).to.be.equal(false);
+                done();
+            }, 0);
+        }, 0);
+    });
+
+    it('should call `onFocus` callback after checkbox-group was focused', (done) => {
+        let onFocus = sinon.spy();
+        let checkBoxGroup = render(
+            <CheckBoxGroup onFocus={ onFocus }>
+                <CheckBox />
+            </CheckBoxGroup>
+        );
+
+        checkBoxGroup.instance.focus();
+
+        setTimeout(() => {
+            expect(onFocus).to.have.been.calledOnce;
+            done();
+        }, 0);
+    });
+
+    it('should call `onBlur` callback after checkbox-group was blured', (done) => {
+        let onBlur = sinon.spy();
+        let checkBoxGroup = render(
+            <CheckBoxGroup onBlur={ onBlur }>
+                <CheckBox />
+            </CheckBoxGroup>
+        );
+
+        checkBoxGroup.instance.focus();
+
+        setTimeout(() => {
+            checkBoxGroup.instance.blur();
+
+            setTimeout(() => {
+                expect(onBlur).to.have.been.calledOnce;
+                done();
+            }, 0);
+        }, 0);
+    });
+
+    it('should call `onChange` callback after checkbox-group was checked', (done) => {
+        let onChange = sinon.spy();
+        let checkBoxGroup = render(
+            <CheckBoxGroup onChange={ onChange }>
+                <CheckBox value='value_1' />
+            </CheckBoxGroup>
+        );
+        let checkboxControlNode = checkBoxGroup.node.querySelector('.checkbox__control');
+
+        simulate(checkboxControlNode, 'change');
+
+        setTimeout(() => {
+            expect(onChange).to.have.been.calledOnce;
+            expect(onChange).to.have.been.calledWith(['value_1']);
+            done();
+        }, 0);
+    });
+
+    it('should disable all child radios when disabled=true', () => {
+        let checkBoxGroup = render(
+            <CheckBoxGroup disabled={ true }>
+                <CheckBox />
+                <CheckBox />
+                <CheckBox />
+            </CheckBoxGroup>
+        );
+
+        let disabledCheckboxNodes = checkBoxGroup.node.querySelectorAll('.checkbox_disabled');
+        expect(disabledCheckboxNodes.length).to.equal(3);
+    });
+
+    it('shouldn\'t call `onChange` callback when disabled=true', function () {
+        let onChange = sinon.spy();
+        let checkboxGroup = render(
+            <CheckBoxGroup onChange={ onChange } disabled={ true }>
+                <CheckBox />
+            </CheckBoxGroup>
+        );
+        let checkbox = checkboxGroup.node.querySelector('.checkbox');
+
+        simulate(checkbox, 'change');
+
+        expect(onChange).to.have.not.been.called;
     });
 });
--- src/checkbox-group/checkbox-group.jsx
+++ src/checkbox-group/checkbox-group.jsx
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { autobind } from 'core-decorators';
 import createFragment from 'react-addons-create-fragment';
 import React from 'react';
 import Type from 'prop-types';
@@ -18,38 +19,92 @@ class CheckBoxGroup extends React.Component {
     static propTypes = {
         /** Тип компонента */
         type: Type.oneOf(['normal', 'button', 'line']),
+        /** Выбранные чекбокс-кнопки */
+        value: Type.arrayOf(Type.oneOfType([Type.string, Type.number])),
+        /** Отображение попапа с ошибкой в момент когда фокус находится на компоненте */
+        width: Type.oneOf(['default', 'available']),
+        /** Уникальное имя блока */
+        name: Type.string,
+        /** Управление возможностью изменения состояния 'checked' дочерних компонентов `CheckBox` */
+        disabled: Type.bool,
         /** Дочерние элементы `CheckBoxGroup`, как правило, компоненты `CheckBox` */
-        children: Type.oneOfType([Type.arrayOf(Type.node), Type.node]),
+        children: Type.node,
         /** Тема компонента */
         theme: Type.oneOf(['alfa-on-color', 'alfa-on-white']),
         /** Дополнительный класс */
         className: Type.oneOfType([Type.func, Type.string]),
         /** Лейбл для группы */
-        label: Type.node
+        label: Type.node,
+        /** Обработчик фокуса радиогруппы */
+        onFocus: Type.func,
+        /** Обработчик снятия фокуса с радиогруппы */
+        onBlur: Type.func,
+        /** Обработчик изменения значения 'checked' одного из дочерних радио-кнопок */
+        onChange: Type.func
+    };
+
+    static defaultProps = {
+        type: 'normal'
+    };
+
+    state = {
+        value: [],
+        focused: false
     };
 
     render(cn) {
         let children = null;
+        let props = { name: this.props.name };
         let checkboxGroupParts = {};
+
+        if (this.props.disabled !== undefined) {
+            props.disabled = this.props.disabled;
+        }
 
         if (this.props.children) {
             children = this.props.children.length ? this.props.children : [this.props.children];
         }
 
+        if (this.props.type === 'button') {
+            props = { ...props, width: this.props.width };
+        }
+
         if (children) {
-            children.forEach((checkbox, index) => {
-                checkboxGroupParts[`checkbox-${index}`] =
-                    (this.props.type !== 'button' && this.props.type !== 'line')
-                        ? <div>{ checkbox }</div>
-                        : checkbox;
+            this.checkboxes = [];
+
+            let value = this.props.value !== undefined
+                ? this.props.value
+                : this.state.value;
+
+            React.Children.forEach(children, (checkbox, index) => {
+                let checkboxNode = React.cloneElement(checkbox, {
+                    ref: checkbox => this.checkboxes.push(checkbox),
+                    checked: checkbox.props.checked !== undefined
+                        ? checkbox.props.checked : value.some(groupValue => groupValue === checkbox.props.value),
+                    onChange: checkbox.props.onChange !== undefined
+                        ? checkbox.props.onChange : checked => this.handleCheckboxChange(checkbox.props.value, checked),
+                    ...props
+                });
+
+                checkboxGroupParts[`checkbox-${index}`] = (this.props.type !== 'button' && this.props.type !== 'line')
+                    ? <div>{ checkboxNode }</div>
+                    : checkboxNode;
             });
         }
 
         return (
             <span
-                className={ `${cn({ type: this.props.type })} control-group` }
+                className={
+                    `${cn({
+                        type: this.props.type,
+                        disabled: props.disabled,
+                        width: props.width ? props.width : null
+                    })} control-group`
+                }
                 role='group'
                 tabIndex='-1'
+                onFocus={ this.handleFocus }
+                onBlur={ this.handleBlur }
             >
                 {
                     !!this.props.label &&
@@ -60,6 +115,66 @@ class CheckBoxGroup extends React.Component {
                 { createFragment(checkboxGroupParts) }
             </span>
         );
+    }
+
+    @autobind
+    handleCheckboxChange(value, checked) {
+        let newValue = this.props.value ? this.props.value.slice() : this.state.value.slice();
+        let changedValueIndex = newValue.findIndex(stateValue => stateValue === value);
+
+        if (checked) {
+            newValue.push(value);
+        } else {
+            newValue.splice(changedValueIndex, 1);
+        }
+
+        this.setState({
+            value: newValue
+        });
+
+        if (this.props.onChange) {
+            this.props.onChange(newValue);
+        }
+    }
+
+    @autobind
+    handleFocus(event) {
+        this.setState({ focused: true });
+
+        if (this.props.onFocus) {
+            this.props.onFocus(event);
+        }
+    }
+
+    @autobind
+    handleBlur(event) {
+        this.setState({ focused: false });
+
+        if (this.props.onBlur) {
+            this.props.onBlur(event);
+        }
+    }
+
+    /**
+     * Устанавливает фокус на первую чекбокс-кнопку в группе.
+     *
+     * @public
+     */
+    focus() {
+        if (this.checkboxes && this.checkboxes[0]) {
+            this.checkboxes[0].focus();
+        }
+    }
+
+    /**
+     * Убирает фокус с группы чекбокс-кнопок.
+     *
+     * @public
+     */
+    blur() {
+        if (document.activeElement) {
+            document.activeElement.blur();
+        }
     }
 }
 

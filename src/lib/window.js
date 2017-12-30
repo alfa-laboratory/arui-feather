ef6c95c1caa95c869ccc1ffb14bdd308c480be5a
Анатолий Бабин <ababin2@alfabank.ru>
2017-12-30 16:39:53 +0300

    refactor(lib/window): add deprecated

--- src/lib/window.js
+++ src/lib/window.js
@@ -1,6 +1,24 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+import deprecated from 'deprecated-decorator';
+
+/**
+ * Check that mouse was outside element.
+ *
+ * @param {Object} event - MouseEvent
+ * @param {Element} element - Element to check bounds
+ * @returns {Boolean}
+ */
+export const isEventOusideBounds = deprecated({
+    name: 'isEventOusideBounds',
+    alternative: 'isEventOutsideClientBounds',
+    version: '12.0.1'
+}, function isEventOusideBounds(event, element) {
+    let rect = element.getBoundingClientRect();
+    return ((event.pageX < rect.left || event.pageX > rect.right) ||
+            (event.pageY < rect.top || event.pageY > rect.bottom));
+});
 
 /**
  * Check that node is outside given element.

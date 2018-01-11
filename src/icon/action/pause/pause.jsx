f4527d7ea1d6079627dcf41e3317070aa79bd8ed
Simon <sima@chetverukhin.ru>
2018-01-11 21:52:11 +0300

    fix(icon): rename component to react.component

--- src/icon/action/pause/pause.jsx
+++ src/icon/action/pause/pause.jsx
@@ -2,10 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { Component } from 'react';
+import React from 'react';
 import Icon from '../../../icon';
 
-class IconPause extends Component {
+class IconPause extends React.Component {
     static propTypes = Icon.propTypes;
 
     render() {
--- src/icon/ui/system-back/system-back.jsx
+++ src/icon/ui/system-back/system-back.jsx
@@ -2,10 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { Component } from 'react';
+import React from 'react';
 import Icon from '../../../icon';
 
-class IconSystemBack extends Component {
+class IconSystemBack extends React.Component {
     static propTypes = Icon.propTypes;
 
     render() {

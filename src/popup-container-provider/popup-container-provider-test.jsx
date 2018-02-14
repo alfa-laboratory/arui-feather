2deb33cc46488b953976f03a24537baa3b3d871c
Mitya Teryaew <mitya.teryaew@gmail.com>
2018-02-14 14:31:01 +0300

    chore(*): test travis

--- src/popup-container-provider/popup-container-provider-test.jsx
+++ src/popup-container-provider/popup-container-provider-test.jsx
@@ -35,18 +35,17 @@ describe('popup-container-provider', () => {
         isInFixedContainer = true,
         renderContainerElement = HTMLElement to which popup will be appended,
         positioningContainerElement = HTMLElement in which popup will try to fit`, () => {
-            let popupContainer = render(
-                <PopupContainerProvider>
-                    <div>Render-test</div>
-                </PopupContainerProvider>
-            );
-            let childContext = popupContainer.instance.getChildContext();
+        let popupContainer = render(
+            <PopupContainerProvider>
+                <div>Render-test</div>
+            </PopupContainerProvider>
+        );
+        let childContext = popupContainer.instance.getChildContext();
 
-            expect(childContext.isInCustomContainer).to.equal(true);
-            expect(childContext.positioningContainerElement).to.equal(popupContainer.node);
-            expect(childContext.renderContainerElement.tagName).to.equal('DIV');
-        }
-    );
+        expect(childContext.isInCustomContainer).to.equal(true);
+        expect(childContext.positioningContainerElement).to.equal(popupContainer.node);
+        expect(childContext.renderContainerElement.tagName).to.equal('DIV');
+    });
 
     it('should render children popups inside itself’s DOM node', () => {
         let popupContainer = render(

a5bee5b9d963c3edf2bb85f1795f999176925c71
Luchansky Oleg <luchansodev@gmail.com>
2018-02-19 00:04:46 +0300

    feat(*): add configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: require.resolve('arui-presets/eslint'),
     rules: {
-        'react/jsx-filename-extension': [2, { 'extensions': ['gemini.js', '.jsx'] }]
+        'react/jsx-filename-extension': [2, { 'extensions': ['gemini.js', '.jsx'] }],
+        'no-mixed-operators': 0
     },
     globals: {
         sinon: true

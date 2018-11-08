5d7d9e293a338fe62832a4ed4cc217720aaab105
Danakt Frost <danakt@protonmail.com>
2018-11-08 13:31:12 +0300

    Commit some files

---
+++ integration-tests/index.test.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import puppeteer from 'puppeteer';
+
+let browser;
+let page;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+        args: [
+            // Required for Docker version of Puppeteer
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            // This will write shared memory files into /tmp instead of /dev/shm,
+            // because Docker’s default for /dev/shm is 64MB
+            '--disable-dev-shm-usage'
+        ]
+    });
+
+    const browserVersion = await browser.version();
+    console.log(`Started ${browserVersion}`);
+});
+
+beforeEach(async () => {
+    page = await browser.newPage();
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
+afterAll(async () => {
+    await browser.close();
+});
+
+describe('App', () => {
+    it('renders', async () => {
+        const response = await page.goto('http://google.com/');
+        assert(response.ok());
+        await page.screenshot({ path: '/screenshots/app.png' });
+    });
+});

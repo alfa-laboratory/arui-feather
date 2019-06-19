8adea0e894e4a365d4efd52548e6d91f9376969e
stepan mikhailyuk <stepan.mikhailyuk@team.wrike.com>
2019-06-19 13:29:24 +0300

    chore(*): try to fix gemini

--- gemini/webpack.gemini.config.js
+++ gemini/webpack.gemini.config.js
@@ -7,9 +7,15 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const WEBPACK_BASE_TEMPLATE = require('arui-presets/webpack.base');
 const WEBPACK_DEV_TEMPLATE = require('arui-presets/webpack.development');
+const path = require('path');
 
 module.exports = merge.smart(WEBPACK_BASE_TEMPLATE, WEBPACK_DEV_TEMPLATE, {
     devtool: 'eval',
+    resolve: {
+        modules: [
+            path.join(__dirname, '../node_modules')
+        ]
+    },
     module: {
         rules: [
             // We need to transpile at least `gemini-react/lib/client-wrapper.js` for IE10

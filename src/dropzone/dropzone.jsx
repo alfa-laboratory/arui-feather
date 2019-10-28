9521df3133b4933e7d326aec5e5f9ead2e4d791f
Aleksey Smirnov <lirien42@gmail.com>
2019-10-29 00:34:07 +0300

    feat(dropzone): main implementation

--- src/dropzone/dropzone.jsx
+++ src/dropzone/dropzone.jsx
@@ -5,19 +5,117 @@
 /* eslint-disable max-len */
 
 import React from 'react';
-// import Type from 'prop-types';
+import Type from 'prop-types';
 
-// import cn from '../cn';
+import cn from '../cn';
 import performance from '../performance';
 
 /**
  * Компонент drag-and-drop контейнер для прикрепления файлов.
  */
-// @cn('dropzone')
+@cn('dropzone')
 @performance()
 class Dropzone extends React.Component {
-    render() {
-        return <div />;
+    static propTypes = {
+        /** Дочерние компоненты */
+        children: Type.node,
+
+        /** Дополнительный класс */
+        className: Type.string,
+
+        /** Идентификатор для систем автоматизированного тестирования */
+        'data-test-id': Type.string,
+
+        /** Уникальный идентификатор блока */
+        id: Type.string,
+
+        /** Уникальное имя блока */
+        name: Type.string,
+
+        /** Тема компонента */
+        theme: Type.oneOf(['alfa-on-color', 'alfa-on-white']),
+
+        /** Текст-подсказка поверх drop-зоны */
+        text: Type.string
+    }
+
+    static defaultProps = {
+        theme: 'alfa-on-white',
+        text: 'Поместите файлы сюда'
+    };
+
+    state = {
+        dragging: false
+    };
+
+    /**
+     * @type {HTMLSpanElement}
+     */
+    root;
+
+    /**
+     * @type {Number}
+     */
+    dragCounter;
+
+    handleDragOver = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    handleDragEnter = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.dragCounter += 1;
+        if (event.dataTransfer.items && event.dataTransfer.items.length > 0) {
+            this.setState({ dragging: true });
+        }
+    };
+
+    handleDragLeave = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.dragCounter -= 1;
+        if (this.dragCounter > 0) return;
+        this.setState({ dragging: false });
+    };
+
+    handleDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.setState({ dragging: false });
+
+        if (event.dataTransfer.files && event.dataTransfer.files.length > 0) {
+            event.dataTransfer.clearData();
+            this.dragCounter = 0;
+        }
+    };
+
+    componentDidMount() {
+        this.dragCounter = 0;
+    }
+
+    render(cn) {
+        return (
+            <div
+                ref={ (root) => { this.root = root; } }
+                id={ this.props.id }
+                data-test-id={ this.props['data-test-id'] }
+                className={ cn() }
+                onDragEnter={ this.handleDragEnter }
+                onDragLeave={ this.handleDragLeave }
+                onDragOver={ this.handleDragOver }
+                onDrop={ this.handleDrop }
+            >
+                <div className={ cn('view', { active: this.state.dragging }) }>
+                    { this.props.text }
+                </div>
+                { this.props.children }
+            </div>
+        );
     }
 }
 
--- src/dropzone/dropzone.test.jsx
+++ src/dropzone/dropzone.test.jsx
@@ -8,9 +8,31 @@ import { mount } from 'enzyme';
 import Dropzone from './dropzone';
 
 describe('dropzone', () => {
+    const filesList = [
+        { name: 'test1.txt', type: 'application/text' },
+        { name: 'test2.txt', type: 'application/text' },
+        { name: 'test3.txt', type: 'application/text' }
+    ];
+    const dataTransfer = {
+        files: filesList,
+        items: filesList,
+        clearData: () => {}
+    };
+
     it('should render without problems', () => {
         let dropzone = mount(<Dropzone />);
 
         expect(dropzone).toMatchSnapshot();
+    });
+
+    it('should set/unset class `dropzone__view_active` on dragEnter', () => {
+        const dropzone = mount(<Dropzone />);
+        const dropzoneView = dropzone.find('.dropzone__view');
+
+        dropzone.simulate('dragenter', { dataTransfer });
+        expect(dropzoneView.getDOMNode().className).toContain('dropzone__view_active');
+
+        dropzone.simulate('dragLeave');
+        expect(dropzone.getDOMNode().className).not.toContain('.dropzone__view_active');
     });
 });

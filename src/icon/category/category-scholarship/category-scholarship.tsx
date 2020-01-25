facc845dc04ade772e3b02292515e32b1cf4486d
Stepan Mikhaylyuk <stepancar@hotmail.com>
2020-01-25 20:57:48 +0100

    chore(*): icon typescript (#960)

---
+++ src/icon/category/category-scholarship/category-scholarship.tsx
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+import Icon from '../../../icon';
+import { IconProps } from '../../../icon/icon';
+
+class IconCategoryScholarship extends React.PureComponent<IconProps> {
+    render() {
+        return (
+            <Icon
+                { ...this.props }
+                name='category-scholarship'
+            />
+        );
+    }
+}
+
+export default IconCategoryScholarship;

ea56c82cb0cf2d28c3aed32e43632e6997e0ec78
stepancar <stepancar@hotmail.com>
2020-01-26 21:11:40 +0300

    chore(*): paragraph to typescript

---
+++ src/paragraph/paragraph.tsx
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+import { createCn } from 'bem-react-classname';
+
+export type ParagraphProps = {
+
+    /**
+     * Тип параграфа
+     */
+    view?: 'lead' | 'normal' | 'small';
+
+    /**
+     * Маркер параграфа
+     */
+    mark?: React.ReactNode;
+
+    /**
+     * Дочерние элементы `Paragraph`
+     */
+    children?: ReadonlyArray<React.ReactNode> | React.ReactNode;
+
+    /**
+     * Тема компонента
+     */
+    theme?: 'alfa-on-color' | 'alfa-on-white';
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Идентификатор компонента в DOM
+     */
+    id?: string;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    'data-test-id'?: string;
+};
+
+/**
+ * Компонент параграфа текста.
+ */
+class Paragraph extends React.PureComponent<ParagraphProps> {
+    cn = createCn('paragraph');
+
+    render() {
+        return (
+            <p
+                className={ this.cn({ view: this.props.view }) }
+                id={ this.props.id }
+                data-test-id={ this.props['data-test-id'] }
+            >
+                { this.props.mark &&
+                    <span className={ this.cn('marker') }>{ this.props.mark }</span>
+                }
+                { this.props.children }
+            </p>
+        );
+    }
+}
+
+export default Paragraph;

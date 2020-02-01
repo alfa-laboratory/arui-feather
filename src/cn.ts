95468f8d327fee6e929f92572357a8bce7e15e75
stepancar <stepancar@hotmail.com>
2020-02-02 01:11:08 +0300

    fix(*): index ts for popup, fix cn types

--- src/cn.ts
+++ src/cn.ts
@@ -5,8 +5,27 @@
 import { createTheme } from 'bem-react-classname';
 import cn from 'cn-decorator';
 
+type CnModifierMap = { [key: string]: boolean | string };
+
+export interface CnFn {
+    (map?: CnModifierMap): string;
+    (elem: string, map?: CnModifierMap): string;
+}
+
+interface ClassDecorator {
+    <TFunction extends Function>(target: TFunction): TFunction | void;
+}
+
+/**
+ * @deprecated
+ */
+const cnDecorator: (blockName: string) => ClassDecorator = (cn as any).create(['alfa-on-white', 'alfa-on-color']);
+
+/**
+ * @deprecated
+ */
+export default cnDecorator;
+
 export type ThemeType = 'alfa-on-color' | 'alfa-on-white';
 
 export const { withTheme, ThemeProvider } = createTheme<ThemeType>('alfa-on-white');
-
-export default cn.create(['alfa-on-white', 'alfa-on-color']);

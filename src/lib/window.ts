d687327b6a44c76f48be012d57e919402d04e9fa
Sergey <tyupaevs@gmail.com>
2020-02-06 20:05:42 +0300

    chore(window): window to typescript (#1024)

--- src/lib/window.ts
+++ src/lib/window.ts
@@ -2,22 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// @ts-nocheck
-
 import deprecated from 'deprecated-decorator';
 
 /**
  * Check that mouse was outside element.
- *
- * @param {Object} event - MouseEvent
- * @param {Element} element - Element to check bounds
- * @returns {Boolean}
  */
 export const isEventOusideBounds = deprecated({
     name: 'isEventOusideBounds',
     alternative: 'isEventOutsideClientBounds',
     version: '12.0.1'
-}, (event, element) => {
+} as any, (event: MouseEvent, element: Element): boolean => {
     const rect = element.getBoundingClientRect();
 
     return ((event.pageX < rect.left || event.pageX > rect.right) ||
@@ -26,23 +20,15 @@ export const isEventOusideBounds = deprecated({
 
 /**
  * Check that node is outside given element.
- *
- * @param {Node} node Node to search
- * @param {Node} element Element that shouldn't contain node
- * @returns {Boolean}
  */
-export function isNodeOutsideElement(node, element) {
+export function isNodeOutsideElement(node: Element, element: Element): boolean {
     return !(element.contains(node) || element === node);
 }
 
 /**
  * Check that mouse was outside element.
- *
- * @param {Object} event - MouseEvent
- * @param {React.Element} element - Element to check bounds
- * @returns {Boolean}
  */
-export function isEventOutsideClientBounds(event, element) {
+export function isEventOutsideClientBounds(event: MouseEvent, element: Element): boolean {
     const rect = element.getBoundingClientRect();
 
     return event.clientX < rect.left || event.clientX > rect.right ||

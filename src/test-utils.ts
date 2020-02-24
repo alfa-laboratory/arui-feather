03b5bf540b4137c8b3e8d986a20775f0d3df57f4
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-02-24 20:43:44 +0300

    fix(test-utils): adds types

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -8,32 +8,43 @@
 /* eslint react/no-render-return-value: 0 */
 /* eslint react/no-find-dom-node: 0 */
 
+import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-dom/test-utils';
+import TestUtils, { Simulate } from 'react-dom/test-utils';
 
 // eslint-disable-next-line no-console
 console.warn('arui-feather/test-utils is deprecated. Use `jest` + `enzyme` instead');
 
-let sharedContainer = null;
+type TestWrapper = {
+    instance: Element;
+    node: Element | Text;
+    container: HTMLElement;
+};
+
+type Options = {
+    container?: HTMLElement;
+};
+
+let sharedContainer: HTMLElement;
 
 /**
  * Тестовая обертка.
  *
- * @typedef {Object} TestWrapper
+ * @typedef {TestWrapper} TestWrapper
  * @property {React.Component} instance Ссылка на экземпляр React компонента.
- * @property {Node} node Корневой HTML узел компонента.
- * @property {Node} container HTML узел контейнера, в котором отрендерен компонент.
+ * @property {HTMLElement} node Корневой HTML узел компонента.
+ * @property {HTMLElement} container HTML узел контейнера, в котором отрендерен компонент.
  */
 
 /**
  * Рендерит компонент в настоящий DOM, возвращает тестовую обертку.
  *
- * @param {Object} jsx JSX для рендера.
- * @param {Object} [options] Опции для рендера.
- * @param {Node} [options.container] Контейнер, в который нарендерить компонент.
+ * @param {React.ReactNode} element JSX для рендера.
+ * @param {Options} [options] Опции для рендера.
+ * @param {HTMLElement} [options.container] Контейнер, в который нарендерить компонент.
  * @returns {TestWrapper}
  */
-export function render(jsx, options = {}) {
+export function render(jsx: React.ReactNode, options: Options = {}): TestWrapper {
     let { container } = options;
 
     if (!container) {
@@ -69,20 +80,20 @@ export function cleanUp() {
 /**
  * Симулирует событие на HTML узле.
  *
- * @param {Node} node HTML узел, на котором необходимо сгенерить событие.
+ * @param {HTMLElement} node HTML узел, на котором необходимо сгенерить событие.
  * @param {String} event Тип события.
- * @param {*} [data] Данные для прокисрования в событие.
+ * @param {*} [eventData] Данные для прокисрования в событие.
  */
-export function simulate(node, event, data) {
-    TestUtils.Simulate[event](node, data);
+export function simulate(node: HTMLElement, eventType: keyof typeof Simulate, eventData: TestUtils.SyntheticEventData) {
+    TestUtils.Simulate[eventType](node, eventData);
 }
 
 /**
  * Обеспечивает сохранение полей события в случае SyntheticEvent.
  *
- * @param {Object} event Событие
+ * @param {React.SyntheticEvent} event Событие
  */
-export function eventPersist(event) {
+export function eventPersist(event: React.SyntheticEvent) {
     if (event.persist) {
         event.persist();
     }

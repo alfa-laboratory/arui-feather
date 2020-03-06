92fc7f607269327f1501c97d2ec3806219a2757a
serov <serov@MacBook-Pro-serov.local>
2020-03-06 15:55:56 +0300

    fix(select): remove double scroll bar

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -346,8 +346,7 @@ export class Popup extends React.Component<PopupProps, PopupState> {
                     size: this.props.size,
                     visible: this.props.visible,
                     height: this.props.height,
-                    padded: this.props.padded,
-                    overflow: !!this.props.maxHeight
+                    padded: this.props.padded
                 }) }
                 id={ this.props.id }
                 style={ {

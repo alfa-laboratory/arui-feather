4d0f7f1f6d78a2f9e04e64eca116a7486ecaf9b5
u_m116y <MVetyugov@alfabank.ru>
2020-07-07 12:26:55 +0300

    feat(icons): add alfa-ui-primitives v2.17.0 and build icons

---
+++ src/icon/banking/online-kassa/online-kassa.tsx
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+import { withTheme } from '../../../cn';
+import Icon from '../..';
+import { IconProps } from '../../icon';
+
+class IconOnlineKassa extends React.PureComponent<IconProps> {
+    render() {
+        return (
+            <Icon
+                { ...this.props }
+                name="online-kassa"
+            />
+        );
+    }
+}
+
+export default withTheme<IconProps, IconOnlineKassa>(IconOnlineKassa);
